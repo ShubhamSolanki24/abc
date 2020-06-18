c7c73a4c30af2788a69b664fe47a73a588b71cb3
ShubhamSolanki24 <49410636+ShubhamSolanki24@users.noreply.github.com>
2020-06-18 23:02:33 +0530

    Update Task0.py

--- Task0.py
+++ Task0.py
@@ -2,17 +2,6 @@
 Read file into texts and calls.
 It's ok if you don't understand how to read files.
 """
-import csv
-with open('texts.csv', 'r') as f:
-    reader = csv.reader(f)
-    texts = list(reader)
-
-with open('calls.csv', 'r') as f:
-    reader = csv.reader(f)
-    calls = list(reader)
-a=int(input("dyydytty"));
-
-
 """
 TASK 0:
 What is the first record of texts and what is the last record of calls?
@@ -20,4 +9,18 @@ Print messages:
 "First record of texts, <incoming number> texts <answering number> at time <time>"
 "Last record of calls, <incoming number> calls <answering number> at time <time>, lasting <during> seconds"
 """
+
+import csv
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    sending, receiving, timestamp = list(reader)[0]
+    print(f'First record of texts, {sending} texts {receiving} at time {timestamp}')
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calling, receiving, timestamp, seconds = list(reader)[-1]
+    print(f'Last record of calls, {calling} calls {receiving} at time {timestamp}, lasting {seconds} seconds')
+    
+
+
 
