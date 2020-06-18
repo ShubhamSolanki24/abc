0b861a208f8161f0220767e0226770edbe6118d1
ShubhamSolanki24 <49410636+ShubhamSolanki24@users.noreply.github.com>
2020-06-18 23:41:52 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -2,15 +2,6 @@
 Read file into texts and calls.
 It's ok if you don't understand how to read files.
 """
-import csv
-with open('texts.csv', 'r') as f:
-    reader = csv.reader(f)
-    texts = list(reader)
-
-with open('calls.csv', 'r') as f:
-    reader = csv.reader(f)
-    calls = list(reader)
-
 
 """
 TASK 1:
@@ -18,3 +9,19 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+import csv
+
+def dubli_list(input):
+    """Remove duplicates from the given list"""
+    return list(set(input))
+numbers = []
+
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    for text in list(reader):
+            numbers.extend(text[:2])
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    for call in list(reader):
+        numbers.extend(call[:2])
