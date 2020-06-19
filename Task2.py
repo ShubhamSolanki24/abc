68dee70c0ae8090eeb61400e93c5c86d792e11ca
ShubhamSolanki24 <49410636+ShubhamSolanki24@users.noreply.github.com>
2020-06-19 23:25:19 +0530

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -3,20 +3,27 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files
 """
 import csv
-with open('texts.csv', 'r') as f:
-    reader = csv.reader(f)
-    texts = list(reader)
+
+"""
+TASK 2:
+The official explanation is incredibly poorly-worded. This is what I've been able to glean from some forum discussions.
+Find the number that spent the most amount of time on a call (calling and receiving combined) in a single month.
+Output the number, the month, and the total time (in seconds) that the number spent on a call in that month:
+"<telephone number> spent the longest time, <total time> seconds, on the phone during September 2016."
+"""
 
 with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
-    calls = list(reader)
+    totals = {}
 
-"""
-TASK 2: Which telephone number spent the longest time on the phone
-during the period? Don't forget that time spent answering a call is
-also time spent on the phone.
-Print a message:
-"<telephone number> spent the longest time, <total time> seconds, on the phone during 
-September 2016.".
-"""
+    for call in list(reader):
+        calling, receiving, timestamp, seconds = call
+        seconds = int(seconds)
 
+        totals[calling] = totals[calling] + seconds if calling in totals else seconds
+        totals[receiving] = totals[receiving] + seconds if receiving in totals else seconds
+
+# Retrieve the key (telephone number) of the largest value (duration)
+phone_number = max(totals.keys(), key=(lambda k: totals[k]))
+
+print(f'{phone_number} spent the longest time, {totals[phone_number]} seconds, on the phone during September 2016.')
