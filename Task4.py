0849b7debfe2d792fb23b6e80c1a3ea1a1bb9580
ShubhamSolanki24 <49410636+ShubhamSolanki24@users.noreply.github.com>
2020-06-19 23:37:50 +0530

    Update Task4.py

--- Task4.py
+++ Task4.py
@@ -3,15 +3,6 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files.
 """
 import csv
-
-with open('texts.csv', 'r') as f:
-    reader = csv.reader(f)
-    texts = list(reader)
-
-with open('calls.csv', 'r') as f:
-    reader = csv.reader(f)
-    calls = list(reader)
-
 """
 TASK 4:
 The telephone company want to identify numbers that might be doing
@@ -25,3 +16,36 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
+
+callers = []
+whitelist = []
+
+
+def dedupe_list(input):
+    """Remove duplicates from the given list"""
+    return list(set(input))
+
+
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    for text in list(reader):
+        sending, receiving, timestamp = text
+        whitelist.extend([sending, receiving])
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    for call in list(reader):
+        calling, receiving, timestamp, seconds = call
+        callers.append(calling)
+        whitelist.append(receiving)
+
+# Deduplicate the lists
+callers = dedupe_list(callers)
+whitelist = dedupe_list(whitelist)
+
+# The list of suspicious numbers is those that appear in the callers list, but not in the whitelist
+suspicious = set(callers).difference(whitelist)
+
+print('These numbers could be telemarketers:')
+for phone_number in sorted(suspicious):
+    print(phone_number)
